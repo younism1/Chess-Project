cc289c5dc880bc69a2f5c05bcb1abbadb4b75726
Mo Younis <mo.younis@rullion.co.uk>
2018-06-15 16:37:26 +0100

    sas

--- specialMove.py
+++ specialMove.py
@@ -7,11 +7,13 @@ def special_move(chess_board, from_coordinates, to_coordinates, undo_move):
         to_row = int(to_coordinates[1])
         from_row = int(from_coordinates[1])
 
-        if chess_board.get(from_coordinates)[2] != "P" and to_row != from_row + 2:
+        if chess_board.get(from_coordinates)[2] == "P" and to_row == from_row + 2:
+            """slap bang between the to and from coordiante"""
+
             return False
         elif chess_board.get(from_coordinates)[2] == "P" and to_row == from_row + 2:
             return True
 
-            
+
 
 
